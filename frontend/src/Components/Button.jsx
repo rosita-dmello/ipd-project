0759fdfe79cdb5a5c5c93f3e0e0df2273bdca8a8
Meet <meetp272003@gmail.com>
2022-12-17 00:22:22 +0530

    Add

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export const Button = ({ content }) => {
+export const Button = ({ content, quiz = 'false' }) => {
   return (
-    <button className="font-poppins font-medium rounded bg-btn px-10 py-2 text-white text-lg md:text-xl tracking-wider hover:shadow-md">
+    <button
+      className="font-poppins font-medium rounded bg-btn px-10 py-2 text-white text-lg md:text-xl tracking-wider hover:shadow-md"
+      disabled={quiz == 'true' ? true : false}
+    >
       {content}
     </button>
   );
