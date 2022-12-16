27a11ee4c71a948763784aa2904ede5f2b5f7b11
Rosita <esorthe7@gmail.com>
2022-12-17 00:30:48 +0530

    integrate

--- frontend/src/Pages/SubmittedPage.jsx
+++ frontend/src/Pages/SubmittedPage.jsx
@@ -21,18 +21,21 @@ export const Result = ({ score, surveyScore }) => {
       audio,
       survey
     };
+    console.log(data)
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await fetch('https://localhost:8000/result', {
+        const response = await fetch('http://localhost:8000/result', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify(data)
         });
+        
         const resData = await response.json();
-        setRes(resData.response.body);
+        console.log(resData);
+        setRes(resData);
         setLoading(false);
         console.log(resData.response.body);
       } catch (err) {
